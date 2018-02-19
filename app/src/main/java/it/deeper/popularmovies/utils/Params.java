0349ebfaba90c26e0dfa7611679fdf5e799868c7
Gianni Cavalli <gianniandreacavalli@gmail.com>
2018-02-19 23:40:51 +0100

    rimossa api_key

--- app/src/main/java/it/deeper/popularmovies/utils/Params.java
+++ app/src/main/java/it/deeper/popularmovies/utils/Params.java
@@ -5,7 +5,7 @@ package it.deeper.popularmovies.utils;
  */
 
 public class Params {
-    public static final String API_KEY = "e027ada806e9927a5c3834746b4cac0f";
+    public static final String API_KEY = "";
     public static final String ENDPOINT = "https://api.themoviedb.org/3/movie/";
     public static final String IMAGE_PATH = "http://image.tmdb.org/t/p/";
     public static final String[] IMAGE_SIZE = {"w92", "w154", "w185",
