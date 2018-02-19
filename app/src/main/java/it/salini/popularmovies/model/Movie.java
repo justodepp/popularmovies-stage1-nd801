58f73eb63b42a78a37ce281143177f09e800dc76
gacavalli <gianni.cavalli@dshare.com>
2018-02-19 14:34:53 +0100

    Initial commit

---
+++ app/src/main/java/it/salini/popularmovies/model/Movie.java
@@ -0,0 +1,59 @@
+package it.salini.popularmovies.model;
+
+public class Movie {
+
+    private int id;
+    private String rating;
+    private String title;
+    private String posterPath;
+    private String overview;
+    private String releaseDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+
+    public void setPosterPath(String posterPath) {
+        this.posterPath = posterPath;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public void setOverview(String overview) {
+        this.overview = overview;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+}
